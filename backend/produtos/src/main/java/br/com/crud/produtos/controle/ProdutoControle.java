abfdcffadf42525a4f03e33e2b48f9882c1d5db7
Dionatabatista <dioanatabatista@gmail.com>
2025-04-01 20:30:12 -0300

    Criação da rota /listar

--- backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
+++ backend/produtos/src/main/java/br/com/crud/produtos/controle/ProdutoControle.java
@@ -1,5 +1,16 @@
 package br.com.crud.produtos.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
+import br.com.crud.produtos.modelo.ProdutoModelo;
+import br.com.crud.produtos.servico.ProdutoServico;
+
 public class ProdutoControle {
   
+  private ProdutoServico ps;
+
+  @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar(){
+        return ps.listar();
+    }
 }
